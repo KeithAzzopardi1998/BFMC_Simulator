c1d090fe9e5f52ab328a13b23a3bad4b023b26b2
keith <keith.azzopardi.16@um.edu.mt>
2021-04-02 15:04:31 +0000

    adding the main object detection code

--- startup_workspace/src/startup_package/src/object_detection.py
+++ startup_workspace/src/startup_package/src/object_detection.py
@@ -1,10 +1,126 @@
 import cv2
-import numpy
+import numpy as np
 import tensorflow as tf
+
+import tflite_runtime.interpreter as tflite
+
 
 class ObjectDetector:
     def __init__(self):
         print("tensorflow GPUs:",tf.config.list_physical_devices('GPU'))
+        #Object Detection interpreter
+        self.od_interpreter = tflite.Interpreter(model_path='./models/object_detector_quant_4.tflite')
+        self.od_interpreter.allocate_tensors()
+        self.od_input_details = self.od_interpreter.get_input_details()
+        self.threshold = 0.3
+
+        #Traffic Sign Recognition interpreter
+        self.tsr_interpreter = tflite.Interpreter(model_path='./models/object_recognition_quant.tflite')
+        self.tsr_interpreter.allocate_tensors()
+        self.tsr_input_details = self.tsr_interpreter.get_input_details()
+        self.tsr_output_details = self.tsr_interpreter.get_output_details()
+        self.threshold = 0.3
     
     def getObjects(self,img):
-        return []
+        image_brightened = self.increase_brightness(img, value=30)
+                
+        obj_list = self.objectDetection(image_brightened)
+
+        #looping through the list of objects, and updating
+        #the class ID of any traffic signs
+        for o in obj_list:
+            #the main OD model uses class 7 for traffic signs
+            if o['class_id']== 7.0:
+                #grab the part of the image containing the sign
+                w = img.shape[1]
+                h = img.shape[0]
+                ymin, xmin, ymax, xmax = o['bounding_box']
+                xmin = int(xmin * w)
+                xmax = int(xmax * w)
+                ymin = int(ymin * h)
+                ymax = int(ymax * h)
+                roi = img[ymin:ymax, xmin:xmax]
+                #run the traffic sign recognition function,
+                #which returns the new class ID
+                o['class_id'] = self.signRecognition(roi)
+        
+        return obj_list
+
+    def set_input_od(self, image):
+        """Sets the input tensor."""
+        tensor_index = self.od_input_details[0]['index']
+        input_tensor = self.od_interpreter.tensor(tensor_index)()[0]
+        input_tensor[:, :] = image
+    
+    def set_input_tsr(self, image):
+        """Sets the input tensor."""
+        tensor_index = self.tsr_input_details[0]['index']
+        input_tensor = self.tsr_interpreter.tensor(tensor_index)()[0]
+        input_tensor[:, :] = image
+
+    def get_output_tensor(self, index):
+        """Returns the output tensor at the given index."""
+        output_details = self.od_interpreter.get_output_details()[index]
+        tensor = np.squeeze(self.od_interpreter.get_tensor(output_details['index']))
+        return tensor
+
+    def make_result(self, box, class_id, scores):
+        result = {
+                    'bounding_box': box,
+                    'class_id': class_id,
+                    'score': scores
+        }
+        return result
+
+    def objectDetection(self, img_in):
+        input_shape = self.od_input_details[0]['shape']
+        _, height, width, _ = input_shape
+
+        resized_image = cv2.resize(img_in, (width, height), interpolation=cv2.INTER_LINEAR)
+
+        resized_image = resized_image[np.newaxis, :]
+
+        self.set_input_od(resized_image)
+
+        self.od_interpreter.invoke()
+
+        boxes = np.clip(self.get_output_tensor(0), 0, 1)
+        classes = self.get_output_tensor(1)
+        scores = self.get_output_tensor(2)
+        count = int(self.get_output_tensor(3))
+
+        results = [self.make_result(boxes[i], classes[i], scores[i]) for i in range(count) if scores[i] >= self.threshold]
+
+        #print(results)
+
+        return results
+
+    def signRecognition(self,img_in):
+        input_shape = self.tsr_input_details[0]['shape']
+        _, height, width, _ = input_shape
+
+        resized_image = cv2.resize(img_in, (width, height), interpolation=cv2.INTER_LINEAR)
+
+        resized_image = resized_image[np.newaxis, :]
+
+        self.set_input_tsr(resized_image)
+
+        self.tsr_interpreter.invoke()
+
+        output_proba = self.tsr_interpreter.get_tensor(self.tsr_output_details[0]['index'])[0]
+        tsr_class = np.argmax(output_proba)
+
+        return float("7.%d"%tsr_class)
+
+    def increase_brightness(self, img, value=30):
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        h, s, v = cv2.split(hsv)
+
+        lim = 255 - value
+        v[v > lim] = 255
+        v[v <= lim] += value
+
+        final_hsv = cv2.merge((h, s, v))
+        img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2RGB)
+
+        return img
